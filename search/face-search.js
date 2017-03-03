0ca2971fe8605f69442a944d3a92850f4e7d6d35
Shinyi Chen <jchen@isi.edu>
2017-03-03 14:34:10 -0800

    Handle post with simple data

--- search/face-search.js
+++ search/face-search.js
@@ -1,30 +1,29 @@
 (function() {
 
-    angular.module('facesearch', ['ui.bootstrap'])
-        .constant("ServerPort", "8000")
+    angular.module('facesearch', ['ui.bootstrap', 'ngUpload'])
+        .constant("searchURL", "http://localhost:8000/search")
 
-        .controller('faceSearchController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+        .controller('faceSearchController', ['$scope', '$rootScope', '$http', 'searchURL', function($scope, $rootScope, $http, searchURL) {
 
+            $scope.isUploading = false;
+
+            $scope.formModel = {
+                "title": ""
+            };
+
+            $scope.message = "";
+
+            $scope.upload = function() {
+                $scope.isUploading = true;
+                $http.post(searchURL, $scope.formModel).then(function(response) {
+                    $scope.message = response.data;
+                    $scope.isUploading  = false;
+                }, function(error) {
+                    console.log(error);
+                });
+            };
 
         }])
-
-        .run(['$http', '$rootScope', 'ServerPort', function($http, $rootScope, ServerPort) {
-
-            var clientHost = window.location.host;
-            var clientHostName = window.location.hostname;
-            var clientOrigin = window.location.origin;
-            var searchURL = clientOrigin.replace(clientHost, clientHostName + ":" + ServerPort) + "/search";
-
-            $rootScope.test = "";
-
-            console.log(searchURL);
-            $http.get(searchURL).then(function(response) {
-                $rootScope.test= response.data;
-            }, function(error) {
-                console.log(error);
-            });
-
-        }]);
 
 
 })();
--- server/search/views.py
+++ server/search/views.py
@@ -7,3 +7,5 @@ from django.http import HttpResponse
 def search(request):
     if request.method == 'GET':
         return HttpResponse("Hello from search")
+    elif request.method == 'POST':
+        return HttpResponse("Hello I got " + request.body)
