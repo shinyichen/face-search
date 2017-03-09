b73c1f6e5bd0a06020be138f0591802449b3bdc6
Shinyi Chen <jchen@isi.edu>
2017-03-08 18:00:15 -0800

    Improved UI

--- search/plot.js
+++ search/plot.js
@@ -1,7 +1,7 @@
 (function() {
 
     angular.module('plotModule', ['ui.bootstrap', 'd3'])
-        .directive('plot', ['d3Service', function (d3Service) {
+        .directive('plot', ['d3Service', '$timeout', function (d3Service, $timeout) {
             return {
                 restrict: 'E',
                 scope: {
@@ -12,11 +12,19 @@
 
                     d3Service.d3().then(function(d3) {
 
-                        var w = (window.innerWidth/12)*9, h = window.innerHeight-150, padding = 50, transform = d3.zoomIdentity;
+                        var w = element[0].parentElement.clientWidth;
+
+                        var h = element[0].offsetParent.clientHeight - element[0].offsetTop - 40;
+
+                        var scaledImageW;
+
+                        var scaledImageH;
 
                         var drawn = false;
 
                         var rect = null;
+
+                        var scale = 1;
 
                         var svg = d3.select(element[0]).append("svg");
                         svg.attr("width", w)
@@ -32,16 +40,16 @@
                             var height = this.height;
 
                             if (width > w || height > h) {
-                                var scale = Math.min(w/width, h/height);
-                                width = width * scale;
-                                height = height * scale;
+                                scale = Math.min(w/width, h/height);
+                                scaledImageW = width * scale;
+                                scaledImageH = height * scale;
                             }
                             var imgs = svg.selectAll("image").data([0]);
                             imgs.enter()
                                 .append("svg:image")
                                 .attr("xlink:href", scope.imagePath)
-                                .attr("width", width)
-                                .attr("height", height);
+                                .attr("width", scaledImageW)
+                                .attr("height", scaledImageH);
                         };
 
                         svg.on("mousedown", function() {
@@ -54,34 +62,15 @@
                             d3.select(window)
                                 .on("mousemove.zoomRect", function() {
                                     var m = d3.mouse(e);
-                                    m[0] = Math.max(0, Math.min(w, m[0]));
-                                    m[1] = Math.max(0, Math.min(h, m[1]));
+                                    m[0] = Math.max(0, Math.min(scaledImageW, m[0]));
+                                    m[1] = Math.max(0, Math.min(scaledImageH, m[1]));
                                     rect.attr("x", Math.min(origin[0], m[0]))
                                         .attr("y", Math.min(origin[1], m[1]))
-                                        .attr("width", Math.abs(m[0] - origin[0]))
-                                        .attr("height", Math.abs(m[1] - origin[1]));
+                                        .attr("width", Math.min(scaledImageW, Math.abs(m[0] - origin[0])))
+                                        .attr("height", Math.min(scaledImageH, Math.abs(m[1] - origin[1])));
                                 }, true)
                                 .on("mouseup.zoomRect", function() {
                                     d3.select(window).on("mousemove.zoomRect", null).on("mouseup.zoomRect", null);
-                                    var m = d3.mouse(e);
-                                    m[0] = Math.max(0, Math.min(w, m[0]));
-                                    m[1] = Math.max(0, Math.min(h, m[1]));
-                                    if (m[0] !== origin[0] && m[1] !== origin[1]) {
-                                        var T = d3.zoomTransform(svg.node()); // current transform
-                                        var dx = Math.abs(m[0] - origin[0])/ T.k;
-                                        var dy = Math.abs(m[1] - origin[1])/ T.k;
-                                        var s = 1 / Math.max(dx/w, dy/h);
-                                        var x = ((m[0]+origin[0])/2 - T.x)/ T.k; // new center
-                                        var y = ((m[1]+origin[1])/2 - T.y)/ T.k;
-                                        x = x * s; // new center with new scale
-                                        y = y * s;
-                                        var tx = w/2 - x;
-                                        var ty = h/2 - y;
-
-                                        //var t = d3.zoomIdentity.translate(tx, ty).scale(s);
-                                        //svg.transition().duration(750).call(zoom.transform, t);
-                                    }
-                                    //rect.remove();
                                     drawn = true;
                                 }, true);
                             d3.event.stopImmediatePropagation();
@@ -100,23 +89,23 @@
 
                         scope.plotApi.getFaceX = function() {
                             if (drawn)
-                                return rect.attr("x");
+                                return rect.attr("x")/scale;
 
                         };
 
                         scope.plotApi.getFaceY = function() {
                             if (drawn)
-                                return rect.attr("y");
+                                return rect.attr("y")/scale;
                         };
 
                         scope.plotApi.getFaceWidth = function() {
                             if (drawn)
-                            return rect.attr("width");
+                            return rect.attr("width")/scale;
                         };
 
                         scope.plotApi.getFaceHeight = function() {
                             if (drawn)
-                            return rect.attr("height");
+                            return rect.attr("height")/scale;
                         };
 
                     });
